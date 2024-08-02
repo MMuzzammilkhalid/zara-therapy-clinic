9b6c7985bb81b4caa88f7d1289ca4a542153d746
MUZZAMMIL <muzammilhadiqa901@gmail.com>
2024-08-02 23:05:47 +0500

    WORK COMPLETE

---
+++ back-to-top-btn.js
@@ -0,0 +1,26 @@
+//Get the button
+var mybutton = document.getElementById("gototop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => { scrollFunction() };
+
+scrollFunction = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+topFunction = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+//   footer
+
+
+
+
